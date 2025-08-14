03d307a772f16ebba8636a5693f7fb509cdd9e97
rodri <rodrihercar24@gmail.com>
2025-08-13 19:55:46 -0600

    Creacion de las clases principales

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 # Actividad de galletas
 
-cookies = []
+
 class Cookie:
     def __init__(self, name, price, weight):
         self.name = name
         self.price = price
         self.weight = weight
 
+    def info(self):
+        print("Informacion de la galleta")
+        print("Datos de la galleta\n"
+              f"Nombre: {self.name}\n"
+              f"Precio: {self.price}\n"
+              f"Peso: {self.weight}\n")
+
+class AddCookies:
+    def __init__(self):
+        self.cookies = {}
     def new(self):
         while True:
             cookie_name = input("Ingresa el nombre de la galleta 🍪: ")
             if cookie_name == "":
                 print("Debes ingresar un nombre a la galleta")
+            elif cookie_name in self.cookies:
+                print("Debes ingresar un nombre a la galleta")
             else:
                 break
         while True:
             try:
-                cookie_price = float(input("Ingrese el precio de la galleta 💵: Q. "))
-                if cookie_price <= 0:
+                cookies_price = float(input("Ingrese el precio de la galleta 💵: Q. "))
+                if cookies_price <= 0:
                     print("Debes ingresar un precio mayor a cero")
                 else:
                     break
@@ -32,13 +44,13 @@ class Cookie:
                     print("Debes ingresar un peso mayor a cero")
                 else:
                     print("Galleta creada")
-                    new_cookie = Cookie(cookie_name, cookie_price, cookie_weight)
-                    cookies.append(new_cookie)
+                    self.cookies[cookie_name] = Cookie(cookie_name, cookies_price, cookie_weight)
                     break
             except ValueError:
                 print("Debes ingresar un numero")
             except Exception as e:
                 print("Ocurrio un error inesperado", e)
+
 
 class ChocOokie(Cookie):
     def __init__(self, name, price, weight, amount):
@@ -51,3 +63,18 @@ class ChocOokie(Cookie):
               f"Precio: {self.price}\n"
               f"Peso: {self.weight}\n"
               f"Cantidad de chispas: {self.amount}")
+
+class CookieStuff:
+    def __init__(self, stuff):
+        self.stuff = stuff
+
+    def stuff_flavor(self):
+        self.stuff = input("Ingrese el sabor de la galleta: ")
+
+class CrambleCookies(Cookie, CookieStuff):
+    def info(self):
+        print("Datos de la galleta\n"
+              f"Nombre: {self.name}\n"
+              f"Precio: {self.price}\n"
+              f"Peso: {self.weight}\n"
+              f"Cantidad de chispas: {self.stuff}")
--- pruebas.py
+++ pruebas.py
@@ -39,3 +39,55 @@ while True:
     except Exception as e:
         print("Ocurrio un error inesperado", e)
 '''
+
+cookies = []
+class Cookie:
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    def info(self):
+        print("Nombre de la galleta: ",self.name)
+        print("Precio: Q. ",self.price)
+        print("Peso: Kg. ",self.weight)
+
+def new():
+    while True:
+        name = input("Ingresa el nombre de la galleta 🍪: ").lower()
+        for c in cookies:
+            if c.name in cookies:
+                print("Debes ingresar un nombre nuevo")
+        if name == "":
+            print("Debes ingresar un nombre a la galleta o crear uno nuevo")
+        else:
+            break
+    while True:
+        try:
+            price = float(input("Ingrese el precio de la galleta 💵: Q. "))
+            if price <= 0:
+                print("Debes ingresar un precio mayor a cero")
+            else:
+                break
+        except ValueError:
+            print("Debes ingresar un numero")
+        except Exception as e:
+            print("Ocurrio un error inesperado", e)
+    while True:
+        try:
+            weight = float(input("Ingrese el peso de la galleta 🏋️: Kg. "))
+            if weight <= 0:
+                print("Debes ingresar un peso mayor a cero")
+            else:
+                print("Galleta creada")
+                new_cookie = Cookie(name, price, weight)
+                cookies.append(new_cookie)
+                break
+        except ValueError:
+            print("Debes ingresar un numero")
+        except Exception as e:
+            print("Ocurrio un error inesperado", e)
+for a in range(2):
+    new()
+    for infor in cookies:
+        infor.info()
